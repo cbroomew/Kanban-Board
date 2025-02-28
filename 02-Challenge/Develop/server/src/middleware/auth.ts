dd6f8b335427482d4b0b2b883f4dfc21cc870635
cbroomew <chad.webster@yahoo.com>
2025-02-28 16:54:04 -0500

    middleware auth logic

--- 02-Challenge/Develop/server/src/middleware/auth.ts
+++ 02-Challenge/Develop/server/src/middleware/auth.ts
@@ -7,4 +7,21 @@ interface JwtPayload {
 
 export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
   // TODO: verify the token exists and add the user data to the request object
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(401).send('Unauthorized');
+  }
+
+  const token = authHeader.split(' ')[1];
+
+  const key = process.env.JWT_SECRET || '';
+
+  try {
+    const decoded = jwt.verify(token, key) as JwtPayload;
+    req.user = decoded as JwtPayload;
+    return next();
+  } catch (error) {
+    return res.status(403).send('Forbidden');
+  }
 };
