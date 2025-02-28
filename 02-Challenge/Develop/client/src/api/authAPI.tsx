3832c9a18d136fade85c3360474b153e72c6a058
cbroomew <chad.webster@yahoo.com>
2025-02-28 17:06:29 -0500

    POST request

--- 02-Challenge/Develop/client/src/api/authAPI.tsx
+++ 02-Challenge/Develop/client/src/api/authAPI.tsx
@@ -2,6 +2,14 @@ import { UserLogin } from "../interfaces/UserLogin";
 
 const login = async (userInfo: UserLogin) => {
   // TODO: make a POST request to the login route
+  const request = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(userInfo),
+  }
+  const response = await fetch('/login', request);
+  const loginInfo = await response.text();
+  return loginInfo;
 }
 
 
