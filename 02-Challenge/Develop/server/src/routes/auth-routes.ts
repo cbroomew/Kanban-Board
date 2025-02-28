e1cd5100c5dfc23bb38c62eaaeed8bda965a3bb6
cbroomew <chad.webster@yahoo.com>
2025-02-28 17:05:34 -0500

    backend routes

--- 02-Challenge/Develop/server/src/routes/auth-routes.ts
+++ 02-Challenge/Develop/server/src/routes/auth-routes.ts
@@ -5,6 +5,22 @@ import bcrypt from 'bcrypt';
 
 export const login = async (req: Request, res: Response) => {
   // TODO: If the user exists and the password is correct, return a JWT token
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ where: { username }});
+    if (!user) {
+      return res.status(404).send('User not found');
+    }
+    const valid = await bcrypt.compare(password, user.password);
+    if (!valid) {
+      return res.status(401).send('Invalid password');
+    }
+    const key = process.env.JWT_SECRET || '';
+    const token = jwt.sign({ username }, key, { expiresIn: '1h' });
+    return res.status(200).send(token);
+  } catch (error) {
+    return res.status(500).send('Internal Server Error');
+  }
 };
 
 const router = Router();
